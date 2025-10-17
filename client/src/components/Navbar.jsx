89b2fd5f5a11b4b0ba06281e56701fed2af81fd8
gituser-30 <aryanmandhare30@gmail.com>
2025-10-17 11:09:13 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
           to="/"
         >
           <img
-            src={head}
+            src="/images/Head_logo.png"
             alt="Dbatu Scholar Hub Logo"
             style={{
               width: "60px",
