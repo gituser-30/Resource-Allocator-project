91fb227b032faee309c50f6145ebe4e749c1a71f
gituser-30 <aryanmandhare30@gmail.com>
2026-01-17 10:28:26 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,7 +6,10 @@ const userSchema = new mongoose.Schema({
   dob: { type: Date, required: true },
   password: { type: String, required: true },
   profilePhoto: { type: String, default: null },
-  Department :{type: String, default:null},
+  department: {
+  type: String,
+  default: "",
+}
   role: { type: String, enum: ["user", "admin"], default: "user" } // ✅ keep for admin panel later
 }, { timestamps: true });
 
